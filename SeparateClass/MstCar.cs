c4c7746be98b5152bfe3fa1f26d9df9b7ea58626
surya16-me <suryaadjie08@gmail.com>
2024-09-19 16:52:22 +0700

    update model

---
+++ SeparateClass/MstCar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateClass56556
+{
+    public class MstCar: BaseCar
+    {
+        public string Engine {  get; set; }        
+    }
+}
---
+++ SeparateClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MstCar car = new MstCar();
+            car.Engine = "electric";
+            car.Name = "EV6";
+            car.Brand = "Kia";
+
+
+        }
+    }
+}
